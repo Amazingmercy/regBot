7c6a017567773bd66f5345a5674c4d52a774df28
Amazingmercy <amaefulemercy499@gmail.com>
2024-08-18 09:53:23 +0100

    removed test file

--- routers/whatsappRoutes.js
+++ routers/whatsappRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const {handleIncomingMessages, verifyWebhook} = require('../controllers/whatsappController')
-const {getFacultyAndStepsById} = require('../test')
 
 
 router.route('/webhook').post(handleIncomingMessages)
@@ -9,16 +8,6 @@ router.route('/webhook').get(verifyWebhook)
 
 
 
-router.get('/test/:id', async (req, res) => {
-    const { id } = req.params;
-    const result = await getFacultyAndStepsById(id);
-    
-    if (result.error) {
-        return res.status(404).json({ message: result.error });
-    }
-
-    res.json(result);
-});
 
 
 
