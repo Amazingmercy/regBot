cb0777d0b80595de10cdcf425e93ca4781328cc7
Amazingmercy <105402025+Amazingmercy@users.noreply.github.com>
2024-09-06 21:50:15 -0700

    Update insertTestData.js

--- services/insertTestData.js
+++ services/insertTestData.js
@@ -1,16 +1,15 @@
-const RegStep = require("../models/regStepModel.js")
-const data = require("./testData.json")
+const RegStep = require("../models/regStepModel.js");
+const data = require("./testData.json");
 
-const insertData = async (){
-  try{
+const insertData = async function() {
+  try {
     await RegStep.insertMany(data);
     console.log('Documents inserted successfully');
   } catch (error) {
     console.error('Error inserting documents:', error);
   }
-}
-
+};
 
 module.exports = {
   insertData
-}
+};
