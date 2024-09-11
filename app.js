32c7d0da11b9d178335c68e6cec18064e14f83a4
Amazingmercy <105402025+Amazingmercy@users.noreply.github.com>
2024-09-11 14:48:02 -0700

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const port = process.env.PORT ||9000;
 const start = async () => {
     try{
         await connectDB(DB_URI)
-        await insertData()
+        //await insertData()
         app.listen(port, console.log(`Server is listening on port ${port}...`));
     } catch (error){
         console.log(error);
